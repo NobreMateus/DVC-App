ae6ea958397598aed39f04b2f24095690d4fc004
Mateus <mateusnobref@gmail.com>
2020-01-03 19:12:40 -0300

    Estilos da aba Desafios ajustados

--- src/components/Challenges.js
+++ src/components/Challenges.js
@@ -7,14 +7,14 @@ export default class Challenges extends React.Component {
   constructor() {
     super();
     this.state = {
-      
+
       c1: false,
       c2: false,
       c3: false,
       c4: false,
       c5: false,
 
-      
+
       nc1: false,
       nc2: false,
       nc3: false,
@@ -22,39 +22,42 @@ export default class Challenges extends React.Component {
       nc5: false
     }
   }
-  cCount= 0;
-  ncCount= 0;
-  
+  cCount = 0;
+  ncCount = 0;
+
   render() {
     return (
       <Container style={styles.container}>
         <Content >
-        <View style={styles.checks}>
-          <Text>Falar de Cristo para 5 não Cristãos</Text>
-          <Text>{this.ncCount}/5</Text>
-          {this.props.nCristhian.map((nC, n)=> {
-            return (
-              <ListItem key={n}>
-                <CheckBox checked={this.state['nc'+(n+1)]} onPress={check => {this.setState({ ['nc'+(n+1)]: !this.state['nc'+(n+1)] }); this.state['nc'+(n+1)]?this.ncCount--: this.ncCount++ }} />
-                <Body>
-                  <Text>{nC}</Text>
-                </Body>
-              </ListItem>
-            )
-          })}
-
-          <Text>Desafiar 5 Cristãos a fazer o mesmo</Text>
-          <Text>{this.cCount}/5</Text>
-          {this.props.cristhian.map((c, n)=> {
-            return (
-              <ListItem key={n}>
-                <CheckBox checked={this.state['c'+(n+1)]} onPress={check => {this.setState({ ['c'+(n+1)]: !this.state['c'+(n+1)] }); this.state['c'+(n+1)]?this.cCount--: this.cCount++ } } />
-                <Body>
-                  <Text>{c}</Text>
-                </Body>
-              </ListItem>
-            )
-          })}
+          <View style={styles.checks}>
+            <View style={styles.menu}>
+              <Text>Falar de Cristo para 5 não Cristãos</Text>
+              <Text>{this.ncCount}/5</Text>
+            </View>
+            {this.props.nCristhian.map((nC, n) => {
+              return (
+                <ListItem key={n} onPress={check => { this.setState({ ['nc' + (n + 1)]: !this.state['nc' + (n + 1)] }); this.state['nc' + (n + 1)] ? this.ncCount-- : this.ncCount++ }}>
+                  <CheckBox checked={this.state['nc' + (n + 1)]} />
+                  <Body>
+                    <Text>{nC}</Text>
+                  </Body>
+                </ListItem>
+              )
+            })}
+            <View style={styles.menu}>
+              <Text>Desafiar 5 Cristãos a fazer o mesmo</Text>
+              <Text>{this.cCount}/5</Text>
+            </View>
+            {this.props.cristhian.map((c, n) => {
+              return (
+                <ListItem key={n} onPress={check => { this.setState({ ['c' + (n + 1)]: !this.state['c' + (n + 1)] }); this.state['c' + (n + 1)] ? this.cCount-- : this.cCount++ }} >
+                  <CheckBox checked={this.state['c' + (n + 1)]} />
+                  <Body>
+                    <Text>{c}</Text>
+                  </Body>
+                </ListItem>
+              )
+            })}
           </View>
         </Content>
       </Container>
@@ -69,7 +72,13 @@ const styles = StyleSheet.create({
     // alignItems: 'center',
     // justifyContent: 'center',
   },
-  checks:{
+  checks: {
     marginHorizontal: "5%"
+  },
+  menu: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 15
   }
 });
