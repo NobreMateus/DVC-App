4592c93f6e06cba38f3a4de845e2b7ec219df0d6
Mateus <mateusnobref@gmail.com>
2020-01-03 18:04:12 -0300

    KeyboardAwareScrollView configurado para rolar a página para a posição do input

--- src/components/DVCFom.js
+++ src/components/DVCFom.js
@@ -1,125 +1,149 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Container, Header, Content, Form, Item, Input, Label, Textarea, Button } from 'native-base';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 export default class DVCForm extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            editMode:true,
+        this.state = {
+            editMode: true,
             name: "",
             phone: "",
-            university:"",
-            vision:"",
-            change:"",
-            promise:"",
-            order:"",
-            cristhian:{
-                c1:"",
-                c2:"",
-                c3:"",
-                c4:"",
-                c5:"",
+            university: "",
+            vision: "",
+            change: "",
+            promise: "",
+            order: "",
+            cristhian: {
+                c1: "",
+                c2: "",
+                c3: "",
+                c4: "",
+                c5: "",
             },
-            ncristhian:{
-                c1:"",
-                c2:"",
-                c3:"",
-                c4:"",
-                c5:"",
+            ncristhian: {
+                c1: "",
+                c2: "",
+                c3: "",
+                c4: "",
+                c5: "",
             }
         }
     }
-    
+
+    _scrollToInput(reactNode) {
+        // Add a 'scroll' ref to your ScrollView
+        this.scroll.props.scrollToFocusedInput(reactNode, 140)
+    }
+
     render() {
         return (
             <Container style={styles.container}>
-                <Content>
+                <KeyboardAwareScrollView
+                    innerRef={ref => {
+                        this.scroll = ref
+                    }}>
                     <Form style={{ ...styles.form, marginTop: 15 }}>
-                        {this.state['editMode']?null:
-                            <Button light onPress={()=>this.enableEditForm()} style={styles.button}><Text style={{...styles.textButton, color:"#a8c5c8"}} > Editar </Text></Button>}
+
+                        {this.state['editMode'] ? null :
+                            <Button light onPress={() => this.enableEditForm()} style={styles.button}><Text style={{ ...styles.textButton, color: "#a8c5c8" }} > Editar </Text></Button>}
+
                         <View style={styles.input} >
                             <Label>Nome</Label>
-                            <Input style={styles.textInput} value={this.state['name']} onChangeText={t => this.setState({ name:t })} disabled={!this.state['editMode']} />
+                            <Input onFocus={event => this._scrollToInput(event.target)} style={styles.textInput} value={this.state['name']} onChangeText={t => this.setState({ name: t })} disabled={!this.state['editMode']} />
                         </View>
+
                         <View style={styles.input} >
                             <Label>Telefone</Label>
-                            <Input style={styles.textInput} value={this.state['phone']} onChangeText={t => this.setState({ phone:t })} disabled={!this.state['editMode']} />
+                            <Input onFocus={event => this._scrollToInput(event.target)} style={styles.textInput} value={this.state['phone']} onChangeText={t => this.setState({ phone: t })} disabled={!this.state['editMode']} />
                         </View>
-                        <View style={styles.input} > 
+
+                        <View style={styles.input} >
                             <Label>Universidade</Label>
-                            <Input style={styles.textInput} value={this.state['university']} onChangeText={t => this.setState({ university:t })} disabled={!this.state['editMode']} />
+                            <Input onFocus={event => this._scrollToInput(event.target)} style={styles.textInput} value={this.state['university']} onChangeText={t => this.setState({ university: t })} disabled={!this.state['editMode']} />
                         </View>
 
                         <View style={styles.input} >
                             <Label>Qual é a sua visão?</Label>
-                            <Textarea rowSpan={5} bordered style={styles.textInput} value={this.state['vision']} onChangeText={t => this.setState({ vision:t })} disabled={!this.state['editMode']}/>
+                            <Textarea onFocus={(event) => {
+                                this._scrollToInput(event.target)
+                            }} rowSpan={5} bordered style={styles.textInput} value={this.state['vision']} onChangeText={t => this.setState({ vision: t })} disabled={!this.state['editMode']} />
                         </View>
+
                         <View style={styles.input} >
                             <Label>Qual a mudança você deseja ver no seu Campus?</Label>
-                            <Textarea rowSpan={5} bordered style={styles.textInput} value={this.state['change']} onChangeText={t => this.setState({ change:t })} disabled={!this.state['editMode']}/>
+                            <Textarea onFocus={event => this._scrollToInput(event.target)} rowSpan={5} bordered style={styles.textInput} value={this.state['change']} onChangeText={t => this.setState({ change: t })} disabled={!this.state['editMode']} />
                         </View>
+
                         <View style={styles.input} >
                             <Label>Leia Mateus 28.18-20. O que Jesus promete?</Label>
-                            <Textarea rowSpan={5} bordered style={styles.textInput} value={this.state['promise']} onChangeText={t => this.setState({ promise:t })} disabled={!this.state['editMode']}/>
+                            <Textarea onFocus={event => this._scrollToInput(event.target)} rowSpan={5} bordered style={styles.textInput} value={this.state['promise']} onChangeText={t => this.setState({ promise: t })} disabled={!this.state['editMode']} />
                         </View>
+
                         <View style={styles.input} >
                             <Label>Leia 2 Timóteo 2.2. O que Paulo está dizendo?</Label>
-                            <Textarea rowSpan={5} bordered style={styles.textInput} value={this.state['order']} onChangeText={t => this.setState({ order:t })} disabled={!this.state['editMode']}/>
+                            <Textarea onFocus={event => this._scrollToInput(event.target)} rowSpan={5} bordered style={styles.textInput} value={this.state['order']} onChangeText={t => this.setState({ order: t })} disabled={!this.state['editMode']} />
                         </View>
+
                         <View style={styles.input} >
                             <Label style={styles.input}>Liste 5 cristãos para fazerem o DVC:</Label>
                             <View style={styles.input}>
-                                <Input style={styles.textInput} value={this.state['cristhian']['c1']} onChangeText={t => this.setState({ cristhian: { ...this.state['cristhian'], c1: t }})} disabled={!this.state['editMode']}/>
+                                <Input onFocus={event => this._scrollToInput(event.target)} style={styles.textInput} value={this.state['cristhian']['c1']} onChangeText={t => this.setState({ cristhian: { ...this.state['cristhian'], c1: t } })} disabled={!this.state['editMode']} />
                             </View>
                             <View style={styles.input}>
-                                <Input style={styles.textInput} value={this.state['cristhian']['c2']} onChangeText={t => this.setState({ cristhian:{ ...this.state['cristhian'], c2: t } })} disabled={!this.state['editMode']}/>
+                                <Input onFocus={event => this._scrollToInput(event.target)} style={styles.textInput} value={this.state['cristhian']['c2']} onChangeText={t => this.setState({ cristhian: { ...this.state['cristhian'], c2: t } })} disabled={!this.state['editMode']} />
                             </View>
                             <View style={styles.input}>
-                                <Input style={styles.textInput} value={this.state['cristhian']['c3']} onChangeText={t => this.setState({ cristhian:{ ...this.state['cristhian'], c3: t } })} disabled={!this.state['editMode']}/>
+                                <Input onFocus={event => this._scrollToInput(event.target)} style={styles.textInput} value={this.state['cristhian']['c3']} onChangeText={t => this.setState({ cristhian: { ...this.state['cristhian'], c3: t } })} disabled={!this.state['editMode']} />
                             </View>
                             <View style={styles.input}>
-                                <Input style={styles.textInput} value={this.state['cristhian']['c4']} onChangeText={t => this.setState({ cristhian:{ ...this.state['cristhian'], c4: t } })} disabled={!this.state['editMode']}/>
+                                <Input onFocus={event => this._scrollToInput(event.target)} style={styles.textInput} value={this.state['cristhian']['c4']} onChangeText={t => this.setState({ cristhian: { ...this.state['cristhian'], c4: t } })} disabled={!this.state['editMode']} />
                             </View>
                             <View style={styles.input}>
-                                <Input style={styles.textInput} value={this.state['cristhian']['c5']} onChangeText={t => this.setState({ cristhian:{ ...this.state['cristhian'], c5: t } })} disabled={!this.state['editMode']}/>
+                                <Input onFocus={event => this._scrollToInput(event.target)} style={styles.textInput} value={this.state['cristhian']['c5']} onChangeText={t => this.setState({ cristhian: { ...this.state['cristhian'], c5: t } })} disabled={!this.state['editMode']} />
                             </View>
                         </View>
+
                         <View style={styles.input} >
                             <Label style={styles.input}>Liste 5 não cristãos para evangelizar:</Label>
                             <View style={styles.input}>
-                                <Input style={styles.textInput} value={this.state['ncristhian']['c1']} onChangeText={t => this.setState({ ncristhian: { ...this.state['ncristhian'], c1: t }})} disabled={!this.state['editMode']}  />
+                                <Input onFocus={event => this._scrollToInput(event.target)} style={styles.textInput} value={this.state['ncristhian']['c1']} onChangeText={t => this.setState({ ncristhian: { ...this.state['ncristhian'], c1: t } })} disabled={!this.state['editMode']} />
                             </View>
                             <View style={styles.input}>
-                                <Input style={styles.textInput} value={this.state['ncristhian']['c2']} onChangeText={t => this.setState({ ncristhian: { ...this.state['ncristhian'], c2: t }})} disabled={!this.state['editMode']}/>
+                                <Input onFocus={event => this._scrollToInput(event.target)} style={styles.textInput} value={this.state['ncristhian']['c2']} onChangeText={t => this.setState({ ncristhian: { ...this.state['ncristhian'], c2: t } })} disabled={!this.state['editMode']} />
                             </View>
                             <View style={styles.input}>
-                                <Input style={styles.textInput} value={this.state['ncristhian']['c3']} onChangeText={t => this.setState({ ncristhian: { ...this.state['ncristhian'], c3: t }})} disabled={!this.state['editMode']}/>
+                                <Input onFocus={event => this._scrollToInput(event.target)} style={styles.textInput} value={this.state['ncristhian']['c3']} onChangeText={t => this.setState({ ncristhian: { ...this.state['ncristhian'], c3: t } })} disabled={!this.state['editMode']} />
                             </View>
                             <View style={styles.input}>
-                                <Input style={styles.textInput} value={this.state['ncristhian']['c4']} onChangeText={t => this.setState({ ncristhian: { ...this.state['ncristhian'], c4: t }})} disabled={!this.state['editMode']}/>
+                                <Input onFocus={event => this._scrollToInput(event.target)} style={styles.textInput} value={this.state['ncristhian']['c4']} onChangeText={t => this.setState({ ncristhian: { ...this.state['ncristhian'], c4: t } })} disabled={!this.state['editMode']} />
                             </View>
                             <View style={styles.input}>
-                                <Input style={styles.textInput} value={this.state['ncristhian']['c5']} onChangeText={t => this.setState({ ncristhian: { ...this.state['ncristhian'], c5: t }})} disabled={!this.state['editMode']}/>
+                                <Input onFocus={(event) => {
+                                    this._scrollToInput(event.target)
+                                }} style={styles.textInput} value={this.state['ncristhian']['c5']} onChangeText={t => this.setState({ ncristhian: { ...this.state['ncristhian'], c5: t } })} disabled={!this.state['editMode']} />
                             </View>
                         </View>
-                        {this.state['editMode']?
-                            <Button primary onPress={()=>{this.saveData()}} style={{...styles.button, color:"#f8a26c"}}><Text style={styles.textButton} > Salvar </Text></Button>
-                            :null}
+
+                        {this.state['editMode'] ?
+                            <Button primary onPress={() => { this.saveData() }} style={{ ...styles.button, color: "#f8a26c", marginBottom: 200 }}><Text style={styles.textButton} > Salvar </Text></Button>
+                            : null}
+
                     </Form>
-                </Content>
+                </KeyboardAwareScrollView>
             </Container>
         );
     }
 
-    saveData(){
-        this.setState({editMode: false }); 
-        this.props.changeNames(this.state);  
+    saveData() {
+        this.setState({ editMode: false });
+        this.props.changeNames(this.state);
     }
 
-    enableEditForm(){
-        this.setState({editMode: true });
+    enableEditForm() {
+        this.setState({ editMode: true });
     }
 }
 
@@ -142,7 +166,7 @@ const styles = StyleSheet.create({
     },
     textInput: {
         backgroundColor: "#fff",
-        borderRadius:8
+        borderRadius: 8
     },
     textButton: {
         color: "#fff",
